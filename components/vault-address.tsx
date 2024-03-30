96f62172eed556db0d0d02f6716fa18795e4936f
ZxStim <thang.victor.luong@gmail.com>
2024-03-30 11:07:12 +0700

    fixed address

--- components/vault-address.tsx
+++ components/vault-address.tsx
@@ -6,7 +6,7 @@ export default function VaultAddressCard() {
     <div className="flex flex-col gap-2 rounded-lg p-4 shadow-xl w-full lg:max-w-3xl">
       <h1 className="text-2xl font-semibold">Vault Address</h1>
       <p className="italic text-sm">Address of the vault on Sepolia testnet</p>
-      <a target="_blank" href={`https://sepolia.etherscan.io/address/0x6cb7C63762d17630B5F46FD16a29bD7c0503a90a`} className="text-sm lg:text-lg font-mono text-blue-500 underline">0x6cb7C63762d17630B5F46FD16a29bD7c0503a90a</a>
+      <a target="_blank" href={`https://sepolia.etherscan.io/address/0x43cd9FdD8594Af5299B96b5e46C5706159f902Ad`} className="text-sm lg:text-lg font-mono text-blue-500 underline">0x43cd9FdD8594Af5299B96b5e46C5706159f902Ad</a>
     </div>
   );
 }
